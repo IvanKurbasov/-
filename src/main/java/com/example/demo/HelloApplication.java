cda1f1f4c555717170c3b2d96a1bbfca382cca0c
Kirill Masalov <kirill.masa@yandex.ru>
2024-05-16 23:45:40 +0300

    комитим2

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -12,6 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         System.out.println(123);
         System.out.println(321);
+        System.out.println(4535345);
     }
 
     public static void main(String[] args) {
